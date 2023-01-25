ddbfc66d08c9fc667d22b00b323b77f455441ab4
nadira <nadibenham@gmail.com>
2023-01-24 20:27:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from "react";
+import Weather from "./Weather";
+
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"> Hello wetaher app</header>
+      <div className="container">
+        <Weather defaultCity="Boston" />
+        <footer>
+          This project is coded by Nadira Benhamida and is{" "}
+          <a
+            href="https://github.com/nadiragit/weather-app-with-react"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
